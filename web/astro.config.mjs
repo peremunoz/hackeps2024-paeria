3eb5d6a91c9318fad0ed42af486440588035e539
Pere <peremunozfiguerol33@gmail.com>
2024-11-23 15:41:19 +0100

    Install react, vercel and tailwind

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+import react from '@astrojs/react'
+
+import vercel from '@astrojs/vercel/serverless'
+
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind()],
+  output: 'server',
+  adapter: vercel(),
+})
